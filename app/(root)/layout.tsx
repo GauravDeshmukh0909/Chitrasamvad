f76ffc51c23709d084d96ded5f50936be4dd7e66
Gaurav Deshmukh <136880099+GauravDeshmukh0909@users.noreply.github.com>
2025-09-20 17:19:44 +0530

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-  title: "connectHub",
+  title: "Chitrasamvad",
   description: "video calling app",
   icons:{
     icon:'/icons/logo.svg'
